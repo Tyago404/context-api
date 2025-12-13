c1a6d412fab9f9d23354b0e12618d2a521cd3c67
Tiago Santos <tiago40427@gmail.com>
2025-12-13 00:51:57 -0300

    perf(Lstorage): adding local storage

--- contexts/color/ColorContext.tsx
+++ contexts/color/ColorContext.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
 type ColorContextType = {
   color: string;
   toggleColor: () => void;
@@ -10,11 +11,25 @@ export const ColorContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
-}) => {
+}) => { 
+ 
   const [color, setColor] = useState("light");
+
+   useEffect(()=>{
+    const savedColor = localStorage.getItem("theme-color")
+    if(savedColor){
+      setColor(savedColor)
+    }
+console.log('executed!')
+  }, [])
+  
+  useEffect(()=>{
+    localStorage.setItem("theme-color", color)
+  },[color])
+
   const toggleColor = () => {
-    setColor(color === "light" ? "dark" : "light");
-    console.log("color has changed!");
+    setColor((prev) => prev === "light" ? "dark" : "light" );
+
   };
   return (
     <ColorContext.Provider value={{ color, toggleColor }}>
